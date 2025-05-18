8efd9c7209a85689cb39bb68d265b180448b90e1
Andrew James <ajames0879@gmail.com>
2025-05-18 16:47:44 -0600

    Adding some vectorization of the images

--- vectorize.py
+++ vectorize.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import json
 
 def image_to_contours(image_path):
     img = Image.open(image_path).convert("RGB")
@@ -11,16 +12,11 @@ def image_to_contours(image_path):
 
     contours_by_color = []
 
-    # Get all unique colors in image
     unique_colors = np.unique(img_np.reshape(-1, 3), axis=0)
 
     for color in unique_colors:
-        # Create a binary mask where this color is white, others black
         mask = np.all(img_np == color, axis=-1).astype(np.uint8) * 255
-
-        # Find contours from the mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
         contours_by_color.append((tuple(color), contours))
 
     return img_np, contours_by_color
@@ -32,18 +28,36 @@ def plot_contours(img_np, contours_by_color):
         for cnt in contours:
             cnt = cnt.squeeze()
             if len(cnt.shape) == 2:
-                plt.plot(cnt[:, 0], cnt[:, 1], linewidth=1, label=str(color))
+                plt.plot(cnt[:, 0], cnt[:, 1], linewidth=1)
     plt.axis("off")
     plt.title("Vectorized Contours")
     plt.show()
 
+def save_contours(contours_by_color, out_path):
+    serializable = []
+    for color, contours in contours_by_color:
+        color_key = [int(c) for c in color]
+        simplified = []
+        for cnt in contours:
+            cnt = cnt.squeeze()
+            if len(cnt.shape) == 2:
+                simplified.append(cnt.tolist())
+        serializable.append({"color": color_key, "contours": simplified})
+    with open(out_path, "w") as f:
+        json.dump(serializable, f)
+    print(f"Saved contours to {out_path}")
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="Path to reduced-color image (from step 1)")
+    parser.add_argument("file", help="Path to reduced-color image")
+    parser.add_argument("--save", type=str, help="Path to save contours JSON")
     args = parser.parse_args()
 
     img_np, contours_by_color = image_to_contours(args.file)
     plot_contours(img_np, contours_by_color)
+
+    if args.save:
+        save_contours(contours_by_color, args.save)
 
 if __name__ == "__main__":
     main()
